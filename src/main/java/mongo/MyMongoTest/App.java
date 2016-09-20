a4ec4b37a47c81cfff8b05b7ebece90598fa6a95
Shi Jijun <5080053@qq.com>
2016-09-20 17:44:44 +0800

    Initial test code for mongo test.

---
+++ src/main/java/mongo/MyMongoTest/App.java
@@ -0,0 +1,13 @@
+package mongo.MyMongoTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
